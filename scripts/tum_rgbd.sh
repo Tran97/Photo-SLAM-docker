7508ec4813b00bbb1e22f8de81850bb281db29ba
Steven <steven.t@outlook.dk>
2024-11-07 11:18:29 +0100

    Updated orb benchmark script

--- scripts/tum_rgbd.sh
+++ scripts/tum_rgbd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-for i in 0 1
+for i in 0 1 2
 do
-RESULT_PATH_FR1 = "../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg1_desk"
+RESULT_PATH_FR1="../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg1_desk"
 mkdir -p $RESULT_PATH_FR1
 ../bin/tum_rgbd_orb \
     ../ORB-SLAM3/Vocabulary/ORBvoc.txt \
@@ -11,43 +11,43 @@ mkdir -p $RESULT_PATH_FR1
     ../cfg/gaussian_mapper/RGB-D/TUM/tum_rgbd.yaml \
     ../../dataset/TUM/rgbd_dataset_freiburg1_desk \
     ../cfg/ORB_SLAM3/RGB-D/TUM/associations/tum_freiburg1_desk.txt \
-    ../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg1_desk \
+    $RESULT_PATH_FR1 \
     no_viewer \
     fr1_run_$i.txt
 
-RESULT_PATH_FR1_ROOM = "../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg1_room"
+RESULT_PATH_FR1_ROOM="../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg1_room"
 mkdir -p $RESULT_PATH_FR1_ROOM
 ../bin/tum_rgbd_orb \
     ../ORB-SLAM3/Vocabulary/ORBvoc.txt \
     ../cfg/ORB_SLAM3/RGB-D/TUM/tum_freiburg1_desk.yaml \
     ../cfg/gaussian_mapper/RGB-D/TUM/tum_rgbd.yaml \
     ../../dataset/TUM/rgbd_dataset_freiburg1_room \
-    ../cfg/ORB_SLAM3/RGB-D/TUM/associations/tum_freiburg1_room.txt \
-    ../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg1_room \
+    ../cfg/ORB_SLAM3/RGB-D/TUM/associations/fr1_room.txt \
+    $RESULT_PATH_FR1_ROOM \
     no_viewer \
     fr1_run_$i.txt
 
-RESULT_PATH_FR2 = "../../results/tum_rgbd_$i/rgbd_dataset_freiburg2_xyz"
+RESULT_PATH_FR2="../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg2_xyz"
 mkdir -p $RESULT_PATH_FR2
-../bin/tum_rgbd \
+../bin/tum_rgbd_orb \
     ../ORB-SLAM3/Vocabulary/ORBvoc.txt \
     ../cfg/ORB_SLAM3/RGB-D/TUM/tum_freiburg2_xyz.yaml \
     ../cfg/gaussian_mapper/RGB-D/TUM/tum_rgbd.yaml \
     ../../dataset/TUM/rgbd_dataset_freiburg2_xyz \
     ../cfg/ORB_SLAM3/RGB-D/TUM/associations/tum_freiburg2_xyz.txt \
-    ../../results/tum_rgbd_$i/rgbd_dataset_freiburg2_xyz \
+    $RESULT_PATH_FR2 \
     no_viewer \
     fr1_run_$i.txt
 
-RESULT_PATH_FR3 = "../../results/tum_rgbd_$i/rgbd_dataset_freiburg3_long_office_household"
+RESULT_PATH_FR3="../../results/orb/tum_rgbd_$i/rgbd_dataset_freiburg3_long_office_household"
 mkdir -p $RESULT_PATH_FR3
-../bin/tum_rgbd \
+../bin/tum_rgbd_orb \
     ../ORB-SLAM3/Vocabulary/ORBvoc.txt \
     ../cfg/ORB_SLAM3/RGB-D/TUM/tum_freiburg3_long_office_household.yaml \
     ../cfg/gaussian_mapper/RGB-D/TUM/tum_rgbd.yaml \
     ../../dataset/TUM/rgbd_dataset_freiburg3_long_office_household \
     ../cfg/ORB_SLAM3/RGB-D/TUM/associations/tum_freiburg3_long_office_household.txt \
-    ../../results/tum_rgbd_$i/rgbd_dataset_freiburg3_long_office_household \
+    $RESULT_PATH_FR3 \
     no_viewer \
     fr3_run_$i.txt
 done
